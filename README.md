e6456298b36d49a9f5dfcba158532f734a11e09d
Walther Chen <walther.chen@gmail.com>
2017-07-17 14:52:58 -0400

    figured out it can't work?

---
+++ README.md
@@ -0,0 +1,4 @@
+Looks like this utility didn't exist ahead of time because it's impossible to
+automatically convert from a simple string url. It's necessary to know the
+structure of the queries, otherwise it's impossible to know which ampersand
+or equal sign is the real delimiter for any query pair.
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use clap::{Arg, App, AppSettings, SubCommand};
 use error::*;
 use url::Url;
 use url::form_urlencoded::byte_serialize;
+use std::collections::HashMap;
 
 fn main() {
     if let Err(ref err) = run() {
@@ -91,6 +92,33 @@ fn encode(s: &str) -> Result<String> {
         out
     })
     .chain_err(|| "No Url found to encode")
+}
+
+fn query_pairs(url: &Url) -> HashMap<String, String> {
+    enum target {
+        Ampersand,
+        EqualSign,
+    }
+    let mut res = HashMap::new();
+
+    // first get just queries
+    let url = url.to_string();
+    if let Some(q_index) = url.find("?") {
+        let queries = url.split(q_index);
+
+        // create index (in reverse) of = and & delimiters
+        let mut cut_indices = Vec::new();
+        let mut query_sub = queries;
+        let mut needle = target::EqualSign;
+
+        while let Some(i) = query_sub.rfind() {
+            cut_indices.push(i);
+        }
+
+        for i in cut_indices.iter().rev().chunk( {
+
+        }
+    }
 }
 
 fn cli_command() -> Result<CommandConfig> {
@@ -167,6 +195,7 @@ pub enum Action {
 
 #[cfg(test)]
 mod test {
+    use url::form_urlencoded::byte_serialize;
     use super::*;
 
     #[test]
@@ -175,6 +204,13 @@ mod test {
         let correct_out = "https://github.com/hw%20chen/aggregate?one=%5Btwo+%3Dthree%5D.%5Bfour%5D&five=six";
         assert_eq!(encode(input).unwrap(), correct_out.to_owned());
 
+        let input = "http://cny-aardvark.datawheel.us:5000/cubes/acs_ygl_language_proficiency_detailed_5/aggregate?drilldown[]=[Year].[Year]&drilldown[]=[English Proficiency].[English Proficiency]&drilldown[]=[Native Language].[Native Language]&cut[]=[Additive Geography].[Tract].&[14000US36053030501]&measures[]=Population Sum&nonempty=true&distinct=false&parents=false&debug=false";
+        let correct_out = "http://cny-aardvark.datawheel.us/cubes/acs_ygl_language_proficiency_detailed_5/aggregate?drilldown%5B%5D=%5BYear%5D.%5BYear%5D&drilldown%5B%5D=%5BEnglish+Proficiency%5D.%5BEnglish+Proficiency%5D&drilldown%5B%5D=%5BNative+Language%5D.%5BNative+Language%5D&cut%5B%5D=%5BAdditive+Geography%5D.%5BTract%5D.%26%5B14000US36053030501%5D=&measures%5B%5D=Population+Sum&nonempty=true&distinct=false&parents=false&debug=false";
+        let url = url::Url::parse(input).unwrap();
+        for (k, v) in url.query_pairs() {
+            println!("query_pairs: {}, {:?}", k, v);
+        }
+        assert_eq!(encode(input).unwrap(), correct_out.to_owned());
     }
 
     #[test]
@@ -182,5 +218,12 @@ mod test {
     fn test_encoding_bad_input() {
         let input = "hw chen/aggregate?one=[two =three].[four]&five=six";
         encode(input).unwrap();
+    }
+
+    #[test]
+    fn test_byte_serialize() {
+        let input = "&one";
+        let correct_out= "%26one";
+        assert_eq!(byte_serialize(input.as_bytes()).collect::<String>(), correct_out.to_owned());
     }
 }
